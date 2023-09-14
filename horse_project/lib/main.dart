8384493835815777ae8d7835e63369eab3adc6c1
Noah <noahcharleshenry4@gmail.com>
2023-09-14 15:55:33 +0200

    Fusion Loging Profile

--- horse_project/lib/main.dart
+++ horse_project/lib/main.dart
@@ -1,33 +1,36 @@
 import 'package:flutter/material.dart';
-//import 'package:horse_project/views/signup_page.dart';
-import 'package:horse_project/views/login_page.dart';
+import 'package:horse_project/views/news_feed_page.dart';
+import 'package:horse_project/controllers/user_controller.dart'; // Import UserController
+import 'package:horse_project/models/user.dart' as appUser; // Import User model
+import 'package:uuid/uuid.dart';
+import 'package:horse_project/views/calendar_page.dart';
+import 'package:horse_project/views/course_programming_page.dart';
+import 'package:horse_project/views/horse_list_page.dart';
+import 'package:horse_project/views/evening_proposal_page.dart';
+import 'package:horse_project/views/profile_page.dart';
 
 void main() {
-  WidgetsFlutterBinding.ensureInitialized();
-  runApp(const MyApp());
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  
-  const MyApp({super.key});
-
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Horse Project',
       theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
+        primarySwatch: Colors.blue,
       ),
-      debugShowCheckedModeBanner: false,
-      home: Scaffold(
-        //body: SignupPage(),
-        body: LoginPage(),
-      ),
+      home: NewsFeedPage(),
+      routes: {
+        '/login': (context) => _LoginPageState(),
+        '/signup': (context) => _SignupPageState(),
+        '/calendar': (context) => CalendarPage(),
+        '/course_programming': (context) => CourseProgrammingPage(),
+        '/horse_list': (context) => HorseListPage(),
+        '/evening_proposal': (context) => EveningProposalPage(),
+        '/profile': (context) => ProfilePage(),
+      },
     );
   }
 }
-
-
-
